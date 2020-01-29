f74849e8106c83d4ecd7e9389865fdf1b20efb6b
Gabriele Carcassi <carcassi@umich.edu>
2020-01-29 10:10:49 -0500

    Adding usage messages

--- bin/cluster-delete.sh
+++ bin/cluster-delete.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Help message
+if [ "$#" -ne 1 ]; then
+    echo "Removes SLATE monitoring from the cluster"
+    echo
+    echo "      $0 CLUSTER_NAME"
+    echo
+    echo "Example:  $0 umich-prod"
+    exit -1
+fi
+
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 source common-cluster.sh || exit 1
 
--- bin/cluster-status.sh
+++ bin/cluster-status.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Help message
+if [ "$#" -ne 1 ]; then
+    echo "Checks SLATE monitoring status on the cluster"
+    echo
+    echo "      $0 CLUSTER_NAME"
+    echo
+    echo "Example:  $0 umich-prod"
+    exit -1
+fi
+
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 source common-cluster.sh || exit 1
 
